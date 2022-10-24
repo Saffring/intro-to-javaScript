61bbee96949b3c870978a877f9b1e1f150eb55d6
Saffrin Granby <saffrin.granby@gmail.com>
2022-10-24 14:12:27 -0700

    Add array file

---
+++ lesson1/arrays.js
@@ -0,0 +1,26 @@
+// This file will help us learn about arrays.
+
+// defining an array of strings.
+let names = ['Vincent', 'Bambi', 'Lucy', 'Richard'];
+
+// defining an array of numbers
+let yearsOfUndergrad = [2016, 2017, 2018, 2019, 2020, 2021];
+
+// defining an empty array
+let emptyArray = [];
+
+// How would I access the third item in names?
+console.log(names[2]); // Lucy
+
+// How do we update arrays?
+
+// Add a new name
+names.push('Bambi II');
+console.log(names); // ['Vincent', 'Bambi', 'Lucy', 'Richard', 'Bambi II']
+
+// Remove the first name
+names.shift(); // Vincent
+
+let myFavoriteDesserts = ['Ice cream', 'White chocolate cookie', 'Lemon cake', 'Creme Brulee'];
+
+
