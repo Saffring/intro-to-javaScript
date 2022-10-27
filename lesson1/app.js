a04af87373160b3c06bbd7a483307b7aa25d6082
Saffrin Granby <saffrin.granby@gmail.com>
2022-10-27 14:47:34 -0700

    Add form even handling

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,22 +1,37 @@
 console.log("connected");
 
+dessertList = [{
+    'name': 'Saffrin',
+    'dessert': 'Carrot Cake'
+},
+{
+    'name': 'Malli',
+    'dessert': 'Ice Cream'
+}
+
+];
+
 let button = document.querySelector("button");
 
 let coffeeLink = document.querySelector("#coffee");
 let codingLink = document.querySelector("#coding");
 let rainLink = document.querySelector("#rain");
 
-let thumbnails = document.querySelectorAll(".thumbnail"); 
+let submitButton = document.querySelector("#submit-button");
 
 let topic = document.querySelector("#topic");
 
+let textInput = document.querySelector("#example-text-input");
+
 // Event listeners
 
+// Give an alert if the "Press me" button is clicked
 button.addEventListener('click', function(){
     alert("You've pressed the button");
 })
 
 
+// Adding an event listener for the three images.
 coffeeLink.addEventListener('click', ()=>{
     addTopicParagraph("coffee")
 })
@@ -50,3 +65,51 @@ function addTopicParagraph(topicName){
         topic.appendChild(node);
     }
 }
+
+// upon submitting the form, we want to collect the information from each input
+submitButton.addEventListener('click', ()=>{
+    // get name from HTML input
+    let name = document.querySelector("#name-input").value ;
+
+    // get preferred dessert from HTML input
+    let preferredDessert = document.querySelector("#dessert").value;
+
+    // add to our dessert list
+    dessertList.push({
+        'name': name,
+        'dessert': preferredDessert
+    });
+
+    // re-generate table with updated list
+    console.log("add?")
+    generateDessertTable(dessertList);
+
+})
+
+function generateDessertTable(desserts){
+    // select table body and remove all the current rows so that we don't add duplicates
+    let tableBody = document.querySelector("#dessert-table-body");
+    tableBody.innerHTML = ""
+
+    let table = document.querySelector("#dessert-table");
+
+    // for each dessert, add a row with the name and the preferred dessert
+    desserts.forEach(dessert => {
+        // create tr element
+        const tr = table.insertRow(0);
+
+        // create td for name and for preferred dessert
+        let name = tr.insertCell(0);
+        let preferredDessert = tr.insertCell(1);
+
+        // set them to the value of the current dessert object in array
+        name.innerHTML = dessert['name'];
+        preferredDessert.innerHTML = dessert['dessert'];
+
+        // append the new row to the table body
+        tableBody.appendChild(tr);
+    });
+}
+
+// run table generation when page is started
+generateDessertTable(dessertList);
