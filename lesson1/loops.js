53ce2cfdabd3e07ffd15d853ae40d70359c7f7dd
Saffrin Granby <saffrin.granby@gmail.com>
2022-11-05 10:18:23 -0700

    Add addtional loops

--- lesson1/loops.js
+++ lesson1/loops.js
@@ -4,3 +4,14 @@
 for (let i=1;i<10;i++) {
     console.log(i);
 }
+
+// looping through a list
+desserts = ['carrot cake', 'kitkat', 'ice cream', 'apple pie']
+for  (let i=0;i<desserts.length;i++) {
+    console.log(desserts[i])
+}
+
+for (const dessert of desserts) {
+    console.log(dessert)
+}
+
