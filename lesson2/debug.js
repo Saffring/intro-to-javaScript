90a8a1f5847bdc1a84a0a7f98eddc844c9fb0b07
Saffrin Granby <saffrin.granby@gmail.com>
2022-11-12 07:48:05 -0800

    Add debug

---
+++ lesson2/debug.js
@@ -0,0 +1,18 @@
+// this function should return the sum of two numbers
+function getSum(a, b){
+    a + b;
+}
+
+// this loop should sum up all values from 0 to 9
+const count = 0;
+for (let i = 0; i < 10; i++) {
+  count = count + i;
+}
+console.log(count);
+
+let firstName = "Jane"
+
+console.log(firtName);
+
+
+
